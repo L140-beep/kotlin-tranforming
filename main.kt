eabb4c2503d0268ce7f51d328cc837c672ba6a4d
L140-beep <kartoshka0302@mail.ru>
2022-10-13 19:52:23 +0800

    added usage examples

--- main.kt
+++ main.kt
@@ -1,19 +1,36 @@
 fun main(){
+
     val triangle = Triangle(Point(1, 2),3F, 4F, 5F)
-    triangle.printSides()
-    triangle.printSidesCount()
     println(triangle.getCenter())
+    triangle.rotate(RotateDirection.CounterClockwise, Point(3, -3))
+    triangle.printSides()
+    println(triangle.getCenter())
+    triangle.rotate(RotateDirection.Clockwise, Point(3, -3))
+    println(triangle.getCenter())
+    triangle.resize(2)
+    println(triangle.getArea())
+
+    println()
 
     val square = Rectangle(Point(4, 3),2F, 4F)
     println(square.getCenter())
-
     square.rotate(RotateDirection.CounterClockwise, Point(3, -3))
     square.printSides()
     println(square.getCenter())
+    square.rotate(RotateDirection.Clockwise, Point(3, -3))
+    println(square.getCenter())
+    square.resize(2)
+    println(square.getArea())
 
+    println()
 
     val circle = Circle(Point(1, 5),3F)
     println(circle.getCenter())
+    circle.rotate(RotateDirection.CounterClockwise, Point(3, -3))
     circle.printSides()
-    circle.printSidesCount()
+    println(circle.getCenter())
+    circle.rotate(RotateDirection.Clockwise, Point(3, -3))
+    println(circle.getCenter())
+    circle.resize(2)
+    println(circle.getArea())
 }
